1ab3bfe462c4e793d474907bba0186fdbd5f4aec
Samuel Reid <sam@Samuels-iMac.attlocal.net>
2020-01-19 14:32:28 -0500

    adding routing

--- oscars2020/src/app/app-routing.module.ts
+++ oscars2020/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserVotesComponent } from './user-votes/user-votes.component';
+import { ResultsTableComponent } from './results-table/results-table.component';
+import { VotesTableComponent } from './votes-table/votes-table.component';
+import { WinnersComponent } from './winners/winners.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: UserLoginComponent },
+  { path: 'cast-votes', component: UserVotesComponent },
+  { path: 'results', component: ResultsTableComponent },
+  { path: 'votes', component: VotesTableComponent },
+  { path: 'winners', component: WinnersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
